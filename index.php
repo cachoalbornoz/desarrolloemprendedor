431d8dfb3ac84e1b1d73c72ace8b3fabc3c358a2
Guillermo Albornoz <cachoalbornoz@gmail.com>
2023-04-05 11:08:44 -0300

    Carrousel de imagenes - Color letras celestes

--- index.php
+++ index.php
@@ -4,9 +4,41 @@
 
 <div class="row">
     <div class="col-xs-12 col-sm-12 col-lg-12">
-        <a href="/desarrolloemprendedor/frontend/programa_jovenes.php">
-            <img src="/desarrolloemprendedor/public/imagenes/prog_jovenes.jpg" class="img-fluid shadow" />
-        </a>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="/desarrolloemprendedor/public/imagenes/prog_jovenes_carrou_1.png" alt="jovenes 1">
+                    <div class="carousel-caption d-none d-md-block">
+
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/desarrolloemprendedor/public/imagenes/prog_jovenes_carrou_2.png" alt="jovenes 2">
+                    <div class="carousel-caption d-none d-md-block">
+
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="/desarrolloemprendedor/public/imagenes/prog_jovenes_carrou_3.png" alt="jovenes 3">
+                    <div class="carousel-caption d-none d-md-block">
+
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Siguiente</span>
+            </a>
+        </div>
     </div>
 </div>
 
@@ -50,25 +82,25 @@
     <div class="col-xs-12 col-md-6 col-lg-3 favenir text-center">
         <a href="/desarrolloemprendedor/frontend/programa_jovenes.php">
             <img src="/desarrolloemprendedor/public/imagenes/financiamiento.png" class="img-fluid" />
-            <span style="color:#1e5571"> FINANCIAMIENTO </span>
+            <span style="color:#09a7e0"> FINANCIAMIENTO </span>
         </a>
     </div>
     <div class="col-xs-12 col-md-6 col-lg-3 favenir text-center">
         <a href="/desarrolloemprendedor/frontend/programa_proaccer.php">
             <img src="/desarrolloemprendedor/public/imagenes/comercializacion.png" class="img-fluid" />
-            <span style="color:#c67d26">COMERCIALIZACIÓN </span>
+            <span style="color:#09a7e0">COMERCIALIZACIÓN </span>
         </a>
     </div>
     <div class="col-xs-12 col-md-6 col-lg-3 favenir text-center">
         <a href="/desarrolloemprendedor/frontend/programa_formacion.php">
             <img src="/desarrolloemprendedor/public/imagenes/formacion.png" class="img-fluid" />
-            <span style="color:#9968bc">FORMACIÓN </span>
+            <span style="color:#09a7e0">FORMACIÓN </span>
         </a>
     </div>
     <div class="col-xs-12 col-md-6 col-lg-3 favenir text-center">
         <a href="/desarrolloemprendedor/frontend/programa_proceder.php">
             <img src="/desarrolloemprendedor/public/imagenes/proceder.png" class="img-fluid" />
-            <span style="color:#72BD8E">INNOVACION</span>
+            <span style="color:#09a7e0">INNOVACION</span>
         </a>
     </div>
 
@@ -94,7 +126,7 @@
 <div class="row">
     <div class="col-xs-12 col-md-6 col-lg-6">
         <a href="frontend/programa_jovenes.php">
-            <h4 style="color:#1e5571" class="favenir text-center mt-5 mb-3">
+            <h4 style="color:#09a7e0" class="favenir text-center mt-5 mb-3">
                 JÓVENES EMPRENDEDORES
             </h4>
         </a>
@@ -111,7 +143,7 @@
     </div>
     <div class="col-xs-12 col-md-6 col-lg-6">
         <a href="/desarrolloemprendedor/frontend/programa_proaccer.php">
-            <h4 style="color:#c67d26" class="favenir text-center mt-5 mb-3">
+            <h4 style="color:#09a7e0" class="favenir text-center mt-5 mb-3">
                 APOYO COMERCIO EMPRENDEDOR
             </h4>
         </a>
@@ -122,7 +154,7 @@
             emprendimientos en marcha. </p>
     </div>
     <div class="col-xs-12 col-md-6 col-lg-6">
-        <h4 style="color:#9968bc" class="favenir text-center mt-5 mb-3">
+        <h4 style="color:#09a7e0" class="favenir text-center mt-5 mb-3">
             TALLERES
         </h4>
         <p class="text-center">
@@ -134,12 +166,12 @@
             emprendedora y de formación para quienes son emprendedores o quieren serlo.</p>
     </div>
     <div class="col-xs-12 col-md-6 col-lg-6">
-        <h4 style="color:#ea1543" class="favenir text-center mt-5 mb-3">
+        <h4 style="color:#09a7e0" class="favenir text-center mt-5 mb-3">
             TUTORÍAS
         </h4>
         <p class="text-center">
             <a href="/desarrolloemprendedor/frontend/programa_tutorias.php">
-                <img src="/desarrolloemprendedor/public/imagenes/tutorias-14-12-21.png" class="img-fluid shadow" />
+                <img src="/desarrolloemprendedor/public/imagenes/tutorias-05-04-23.png" class="img-fluid shadow" />
             </a>
         </p>
         <p class="text-center text-uppercase pl-5 pr-5">Para emprendedoras /es con proyecto en marcha. Participá de un
@@ -147,12 +179,12 @@
     </div>
     <div class="col-xs-12 col-md-6 col-lg-6">
         <a href="frontend/programa_proceder.php">
-            <h4 style="color:#72BD8E" class="favenir text-center mt-5 mb-3">
+            <h4 style="color:#09a7e0" class="favenir text-center mt-5 mb-3">
                 EMPRENDIMIENTOS DINÁMICOS
             </h4>
         </a>
         <p class="text-center">
-            <img src="/desarrolloemprendedor/public/imagenes/emprendimientos-dinamicos.png" class="img-fluid shadow" />
+            <img src="/desarrolloemprendedor/public/imagenes/emprendimientos-dinamicos05-04-23.png" class="img-fluid shadow" />
         </p>
 
         <p class="text-center text-uppercase pl-5 pr-5">
@@ -162,7 +194,7 @@
 
     <div class="col-xs-12 col-md-6 col-lg-6">
         <a href="https://www.entrerios.gov.ar/economicoemprendedor/mipymes">
-            <h4 style="color:#FF5733" class="favenir text-center mt-5 mb-3">
+            <h4 style="color:#09a7e0" class="favenir text-center mt-5 mb-3">
                 CONSOLIDACION PRODUCTIVA
             </h4>
         </a>
@@ -178,6 +210,58 @@
 </div>
 
 
+<div class="row mt-5 mb-3 favenir">
+    <div class="col-xs-12 col-sm-12 col-lg-12">
+        <h2 class=" mt-5 mb-5">Trayectorías de Jóvenes Emprendedores </h2>
+
+        <div class="row mt-3">
+            <div class="col-6">
+                <h4 class="mb-3">Caravan Café y Crepes</h4>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="https://www.youtube.com/embed/Dt8ogUJgvzA" title="Caravan Café y Crepes" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-6">
+                <h4 class="mb-3">Cocomelón</h4>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="https://www.youtube.com/embed/H1Qjf5uD3yM" title="Cocomelón" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row mt-3">
+            <div class="col-6">
+                <h4 class="mt-5 mb-3">Micro Digital Tecnología Forense</h4>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="https://www.youtube.com/embed/uUIdZDCtF-A" title="Micro Digital Tecnología Forense" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-6">
+                <h4 class="mt-5 mb-3">Luthier</h4>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/sFd9hsyyAWM" title="Luthier" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-5">
+            <div class="col-6">
+                <h4 class="mt-5 mb-3">Eco Diseño Paraná</h4>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="https://www.youtube.com/embed/EY_GwrvaFtg" title="Eco Diseño Paraná" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-6">
+                <h4 class="mt-5 mb-3">Cooperativa de Trabajo Tenondé Limitada</h4>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/W3crBn2yzgk" title="Cooperativa de Trabajo Tenondé Limitada" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
 
 <div class="row">
     <div class="col-xs-12">
@@ -210,3 +294,4 @@
 
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/desarrolloemprendedor/accesorios/footer.php';
+?>
