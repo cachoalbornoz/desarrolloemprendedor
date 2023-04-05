53c89e3effbb8b60753d8da9cf1cee8f229bd8ef
Guillermo Albornoz <cachoalbornoz@gmail.com>
2023-04-05 15:57:25 -0300

    Cambio color semi esferas y titulos

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
     </div>
 </div>
 
-<div class="row">
+<div class="row" >
     <div class="col-xs-12 col-md-6 col-lg-3 favenir text-center">
         <a href="/desarrolloemprendedor/frontend/programa_jovenes.php">
             <img src="/desarrolloemprendedor/public/imagenes/financiamiento.png" class="img-fluid" />
@@ -87,19 +87,19 @@
     </div>
     <div class="col-xs-12 col-md-6 col-lg-3 favenir text-center">
         <a href="/desarrolloemprendedor/frontend/programa_proaccer.php">
-            <img src="/desarrolloemprendedor/public/imagenes/comercializacion.png" class="img-fluid" />
+            <img src="/desarrolloemprendedor/public/imagenes/financiamiento.png" class="img-fluid" />
             <span style="color:#09a7e0">COMERCIALIZACIÓN </span>
         </a>
     </div>
     <div class="col-xs-12 col-md-6 col-lg-3 favenir text-center">
         <a href="/desarrolloemprendedor/frontend/programa_formacion.php">
-            <img src="/desarrolloemprendedor/public/imagenes/formacion.png" class="img-fluid" />
+            <img src="/desarrolloemprendedor/public/imagenes/financiamiento.png" class="img-fluid" />
             <span style="color:#09a7e0">FORMACIÓN </span>
         </a>
     </div>
     <div class="col-xs-12 col-md-6 col-lg-3 favenir text-center">
         <a href="/desarrolloemprendedor/frontend/programa_proceder.php">
-            <img src="/desarrolloemprendedor/public/imagenes/proceder.png" class="img-fluid" />
+            <img src="/desarrolloemprendedor/public/imagenes/financiamiento.png" class="img-fluid" />
             <span style="color:#09a7e0">INNOVACION</span>
         </a>
     </div>
@@ -216,13 +216,13 @@
 
         <div class="row mt-3">
             <div class="col-6">
-                <h4 class="mb-3">Caravan Café y Crepes</h4>
+                <h4 class="mb-3" style="color:#09a7e0">Caravan Café y Crepes</h4>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe src="https://www.youtube.com/embed/Dt8ogUJgvzA" title="Caravan Café y Crepes" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
             <div class="col-6">
-                <h4 class="mb-3">Cocomelón</h4>
+                <h4 class="mb-3" style="color:#09a7e0">Cocomelón</h4>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe src="https://www.youtube.com/embed/H1Qjf5uD3yM" title="Cocomelón" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
@@ -232,13 +232,13 @@
 
         <div class="row mt-3">
             <div class="col-6">
-                <h4 class="mt-5 mb-3">Micro Digital Tecnología Forense</h4>
+                <h4 class="mt-5 mb-3" style="color:#09a7e0">Micro Digital Tecnología Forense</h4>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe src="https://www.youtube.com/embed/uUIdZDCtF-A" title="Micro Digital Tecnología Forense" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
             <div class="col-6">
-                <h4 class="mt-5 mb-3">Luthier</h4>
+                <h4 class="mt-5 mb-3" style="color:#09a7e0">Luthier</h4>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/sFd9hsyyAWM" title="Luthier" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
@@ -247,13 +247,13 @@
 
         <div class="row mt-3 mb-5">
             <div class="col-6">
-                <h4 class="mt-5 mb-3">Eco Diseño Paraná</h4>
+                <h4 class="mt-5 mb-3" style="color:#09a7e0">Eco Diseño Paraná</h4>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe src="https://www.youtube.com/embed/EY_GwrvaFtg" title="Eco Diseño Paraná" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
             <div class="col-6">
-                <h4 class="mt-5 mb-3">Cooperativa de Trabajo Tenondé Limitada</h4>
+                <h4 class="mt-5 mb-3" style="color:#09a7e0">Cooperativa de Trabajo Tenondé Limitada</h4>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/W3crBn2yzgk" title="Cooperativa de Trabajo Tenondé Limitada" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
