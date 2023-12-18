89ba0b5ea19543025e2c5081c80a85f051271c8c
Guillermo Albornoz <cachoalbornoz@gmail.com>
2023-12-18 16:33:52 -0300

    Ubicacion del expediente en el listado de Morosos

--- expedientes/detalle_morosos.php
+++ expedientes/detalle_morosos.php
@@ -40,7 +40,7 @@
         "dom"           : '<"wrapper"Brflitp>', 
         "info"          : true,
         "buttons"       : ['copy', 'excel', 'pdf',  'colvis'],
-        "order"         : [[ 1, "asc" ]],
+        "order"         : false,
         "stateSave"     : true,
         "columnDefs"    : [{}, ],
         "language"      : { "url": "../public/DataTables/spanish.json" }
@@ -53,6 +53,7 @@
     <thead>
     <tr>
         <th>#</th>
+        <th>Año</th>
         <th>Apellido</th>
         <th>Nombres</th>
         <th>Localidad</th>
@@ -65,7 +66,8 @@
     <tbody>
     <?php
 
-    $tabla_expedientes = mysqli_query($con, "SELECT edc.id_expediente, exped.nro_exp_control, exped.nro_proyecto, emp.apellido, emp.nombres, loca.nombre as localidad, emp.cod_area, emp.celular, emp.telefono, emp.email
+    $tabla_expedientes = mysqli_query($con, "SELECT edc.id_expediente, exped.nro_exp_control, exped.nro_proyecto, emp.apellido, emp.nombres, loca.nombre as localidad, 
+        emp.cod_area, emp.celular, emp.telefono, emp.email, year(exped.fecha_otorgamiento) AS aotorga
         FROM expedientes exped
         INNER JOIN expedientes_detalle_cuotas edc ON exped.id_expediente = edc.id_expediente
         INNER JOIN rel_expedientes_emprendedores rel_exp ON exped.id_expediente = rel_exp.id_expediente
@@ -73,7 +75,7 @@
         INNER JOIN localidades AS loca ON emp.id_ciudad = loca.id
         WHERE exped.estado = 2
         GROUP BY edc.id_expediente
-        ORDER BY emp.apellido, emp.nombres");
+        ORDER BY year(exped.fecha_otorgamiento), emp.apellido, emp.nombres");
 
     $total = 0;
 
@@ -82,26 +84,42 @@
         $id_expediente = $fila['id_expediente'];
         $id_proyecto = $fila['nro_proyecto'];
 
-        ?>
-        <tr>
-            <td class="text-center">
-                <a href="sesion_usuario_expediente.php?id=<?php echo $id_expediente; ?>&id_proyecto=<?php echo $id_proyecto; ?>" title="Ver expediente">
-                    <?php echo $fila['nro_proyecto']; ?>
-                </a>                
-            </td>
-            <td><?php echo $fila['apellido']; ?></td>
-            <td><?php echo $fila['nombres']; ?></td>
-            <td><?php echo $fila['localidad']; ?></td>
-            <td class="text-center"><?php echo $fila['cod_area']; ?></td>
-            <td class="text-center"><?php echo $fila['celular']; ?></td>
-            <td class="text-center"><?php echo $fila['telefono']; ?></td>
-            <td><?php echo $fila['email']; ?></td>
-        </tr>
-        <?php
+        $tabla_ubicacion = mysqli_query($con, "SELECT tu.id_ubicacion
+            FROM rel_expedientes_ubicacion reu
+            INNER JOIN expedientes_ubicaciones eu ON eu.id_ubicacion = reu.id_ubicacion
+            INNER JOIN tipo_ubicaciones tu ON eu.id_tipo_ubicacion = tu.id_ubicacion
+            WHERE reu.id_expediente = $id_expediente
+            ORDER BY eu.fecha DESC LIMIT 1;");
+
+        if($registro_ubicacion = mysqli_fetch_array($tabla_ubicacion)){
+
+            if($registro_ubicacion[0] == 1){
+
+            ?>
+            <tr>
+                <td class="text-center">
+                    <a href="sesion_usuario_expediente.php?id=<?php echo $id_expediente; ?>&id_proyecto=<?php echo $id_proyecto; ?>" title="Ver expediente">
+                        <?php echo $fila['nro_proyecto']; ?>
+                    </a>                
+                </td>
+                <td><?php echo $fila['aotorga']; ?></td>
+                <td><?php echo $fila['apellido']; ?></td>
+                <td><?php echo $fila['nombres']; ?></td>
+                <td><?php echo $fila['localidad']; ?></td>
+                <td class="text-center"><?php echo $fila['cod_area']; ?></td>
+                <td class="text-center"><?php echo $fila['celular']; ?></td>
+                <td class="text-center"><?php echo $fila['telefono']; ?></td>
+                <td><?php echo $fila['email']; ?></td>
+            </tr>
+            <?php
+            }
+
+        }
     }
     ?>
     </tbody>
     <tr>
+        <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
