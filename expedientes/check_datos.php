943bd1369eb96b8a2afeac3978b04bf024c5a347
Guillermo Albornoz <cachoalbornoz@gmail.com>
2023-12-19 16:57:38 -0300

    Actualizacion de Ubicaciones

--- expedientes/check_datos.php
+++ expedientes/check_datos.php
@@ -4,15 +4,14 @@
     $con=conectar();
 
     // BUSCA EXPEDIENTES SIN UBICACIONES
-    $tabla_check = mysqli_query($con, "SELECT reu.id_expediente, exped.fecha_otorgamiento
-        FROM expedientes exped 
-        LEFT JOIN rel_expedientes_ubicacion reu ON exped.id_expediente = reu.id_expediente
-        WHERE reu.id_expediente IS NOT NULL 
-        AND exped.nro_exp_madre <> 0 
-        AND reu.id_ubicacion = 0");
+    $tabla_check = mysqli_query($con, "SELECT exped.id_expediente, exped.fecha_otorgamiento, reu.id_ubicacion  
+    FROM expedientes exped 
+    INNER JOIN rel_expedientes_ubicacion reu ON exped.id_expediente = reu.id_expediente
+    LEFT JOIN expedientes_ubicaciones eu ON reu.id_ubicacion = eu.id_ubicacion
+    WHERE YEAR(exped.fecha_otorgamiento) = 2023 AND eu.id_ubicacion IS NULL 
+    ORDER BY YEAR(exped.fecha_otorgamiento)");
 
     echo "Expedientes encontrados :" .mysqli_num_rows($tabla_check)."<br>";
-
 
     $total = 0;
 
@@ -20,6 +19,7 @@
 
         $id_expediente      = $fila_check['id_expediente'];
         $fecha_otorgamiento = $fila_check['fecha_otorgamiento'];
+        $id_ubicacion_ant   = $fila_check['id_ubicacion'];
 
         // /////////////////////////////////////////////////////////// EXPEDIENTES - UBICACIONES
 
@@ -30,15 +30,10 @@
         $id_relacion = mysqli_query($con, "INSERT INTO rel_expedientes_ubicacion (id_expediente, id_ubicacion) values ($id_expediente, $id_ubicacion)") 
             or die('Revisar insercion relac expedientes - ubicaciones');
 
+        mysqli_query($con, "DELETE FROM rel_expedientes_ubicacion WHERE id_expediente = $id_expediente AND id_ubicacion = $id_ubicacion_ant") 
+            or die('Revisar insercion relac expedientes - ubicaciones');            
+
         $total++;
-
-
-        echo 'ID_EXPEDIENTE :'. $id_expediente. '<br>';
-        echo 'FECHA_EXPEDIENTE :'. $fecha_otorgamiento. '<br>';
-        echo 'ID_UBICACION :'. $id_ubicacion . '<br>';
-        echo 'ID_RELACION :'. $id_relacion;
-
-        exit;
         
     }
 
