971e5f86e0d9ee8cb7b74d3d26deec52537b43e3
Guillermo Albornoz <cachoalbornoz@gmail.com>
2024-03-28 18:15:59 -0300

    Create composer dependences files

--- desarrollo/desarrollo.php
+++ desarrollo/desarrollo.php
@@ -44,7 +44,7 @@ $con = conectar();
                     $mensaje      = "<p> Estimada/o <strong> $nombres, $apellido </strong> </p> <p> Te informamos que el período de prórroga ha finalizado y el próximo mes de <strong> $proximo_mes </strong> comienzan los vencimientos de cuotas de tu Crédito. </p> <p> Saludos </p>";
                     $logo         = null;
 
-                    //enviar($email, $asunto, $destinatario, $mensaje, $logo);
+                    enviar($email, $asunto, $destinatario, $mensaje, $logo);
 
                     $query = 'SELECT edc.id_expediente, edc.fecha_vcto, edc.estado, emp.apellido, emp.nombres, emp.email
                     FROM expedientes exped
